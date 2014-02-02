a194425a756ec7f4e51cfaf3d41bd27d6a1df32a
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-02 10:34:23 -0800

    Object oriented the paddles

--- pong.js
+++ pong.js
@@ -1,25 +1,22 @@
-/*
-This file declares all the variables, as well as their default values
-*/
+//Constructor for paddles
+function paddle(element_id, y_df, dy_df, speed_df, height_df) {
+	this.style = document.getElementById(element_id);
 
-//Default Values for variables
-var paddle_speed_df = 2.0;
-var paddle_height_df = 40;
+	this.y = y_df;
+	this.dy = dy_df;
+	this.speed = speed_df;
+	this.height = height_df;
 
-//Paddles are initialized
-var p1 = document.getElementById("paddle1");
-var p2 = document.getElementById("paddle2");
-//Paddle characteristics
-var p1_y = 230; //where it is positioning wise
-var p2_y = 230;
-var p1_dy = 0;
-var p2_dy = 0;
-var paddle_speed = paddle_speed_df;
-var paddle_height = paddle_height_df; //paddle height defined in pixels
-var paddle2_height = paddle_height_df;
-var paddle2_speed = paddle_speed_df;
+	this.set = paddleSet;
+}
 
-//Set up the ball
+function paddleSet(paddle_settings) {
+	this.height = paddle_settings[0].value;
+}
+
+var p1 = new paddle("paddle1", 230, 0, 2.0, 40);
+var p2 = new paddle("paddle2", 230, 0, 2.0, 40);
+
 //Constructor for Ball Object
 function ball(element_id, spawn_x_df, spawn_y_df, dx_df, dy_df, inc_df) {
 	this.style = document.getElementById(element_id).style;
@@ -41,12 +38,9 @@ function ball(element_id, spawn_x_df, spawn_y_df, dx_df, dy_df, inc_df) {
 	this.inc = inc_df;
 }
 
-ball = new ball(247, 247, 0.8, 1.1, 0.0000000000001)
-var ball = document.getElementById("ball");
-var ball_y = spawn_y;
-var ball_dx = ball_dx_df;
-var dy = dy_df;
-var ball_inc = ball_inc_df; //how much the ball accelerates over time
+//Creates the ball
+var ball = new ball("ball", 247, 247, 0.8, 1.1, 0.0000000000001)
+
 var ball_miss; //counts how many times the ball has been missed by a particular player
 
 var p1_score = 0;
@@ -93,13 +87,11 @@ function convertInput(input, default_val) {
 	}
 }
 
-function paddleSet(paddle_settings) {
 
-}
 function newGame() {
 	//gets input from form
 	ball_settings = document.getElementsByName("ball_settings");
-	ball_dx = ball_settings[0].value;
+	ball.dx = ball_settings[0].value;
 
 	paddle1_settings = document.getElementsByName("paddle1_settings");
 	paddle_height = paddle1_settings[0].value;
@@ -125,50 +117,49 @@ function newGame() {
 }
 
 function gameTick() {
-	ball_x += ball_dx;
-	ball_y += dy;
+	ball.x += ball.dx;
+	ball.y += ball.dy;
 	//makes sure paddle doesn't go off screen
 	p1_y = Math.min(Math.max(p1_y + p1_dy, 0), 460);
 	p2_y = Math.min(Math.max(p2_y + p2_dy, 0), 460);
 	
 	p1.style.top=p1_y;
 	p2.style.top=p2_y;
-	ball.style.top=ball_y;
-	ball.style.left=ball_x;
 	
 	var offset = Math.floor(Math.random()*50);
 
-
 	//bounces the ball off the paddle or wall
-	if (ball_y > 489 || ball_y < 1) {
+	if (ball.y > 489 || ball.y < 1) {
 		dy *= -1;
 	}
-	if (ball_x >= 472 && ball_y >= p2_y && ball_y <= p2_y + 40) {
-		ball_dx *= -1;
+	if (ball.x >= 472 && ball.y >= p2_y && ball.y <= p2_y + 40) {
+		ball.dx *= -1;
 		ball_miss = 0;
 	}
-	if (ball_x <= 16 && ball_y >= p1_y && ball_y <= p1_y + 40) {
-		ball_dx *= -1;
+	if (ball.x <= 16 && ball.y >= p1_y && ball.y <= p1_y + 40) {
+		ball.dx *= -1;
 		ball_miss = 0;
 	}
 	//if ball goes out of bounds, gives pts and respawns
-	if(ball_x > 490) {
+	if(ball.x > 490) {
  		p1_score += 1;
 		//makes the serve distance longer, randomizes spawn
- 		ball_x = ball_spawn_x-offset;
- 		ball_y = spawn_y;
+ 		ball.x = ball.spawn_x-offset;
+ 		ball.y = ball.spawn_y;
 		document.getElementById("player1-score").innerHTML = p1_score;
 		ball_miss +=1;
 	}
- 	if(ball_x < 10) {
+ 	if(ball.x < 10) {
     	p2_score += 1;
 		//makes the serve distance longer
-		ball_x = ball_spawn_x+offset;
-		ball_y = spawn_y;
+		ball.x = ball.spawn_x+offset;
+		ball.y = ball.spawn_y;
 		document.getElementById("player2-score").innerHTML = p2_score;
 		ball_miss += 1;
 	}
-	(ball_dx<0)? ball_dx-=ball_inc:ball_dx+=ball_inc;
+	(ball.dx<0)? ball.dx-=ball_inc:ball.dx+=ball_inc;
+	ball.style.top=ball.y;
+	ball.style.left=ball.x;
 	setTimeout('gameTick()', 5);
 }
 
