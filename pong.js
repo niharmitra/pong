9eb273a6ce838449b0158cdd9e17ecd3471db82a
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-02 14:09:03 -0800

    More fixes

--- pong.js
+++ pong.js
@@ -129,13 +129,13 @@ function gameTick() {
 	if (ball.y > 489 || ball.y < 1) {
 		ball.dy *= -1;
 	}
-	if (ball.x >= 472 && ball.y >= p2.y && ball.y <= p2.y + 40) {
+	if (ball.x >= 472 && ball.y >= p2.y && ball.y <= p2.y + p2.height) {
 		ball.dx *= -1;
-		ball_miss = 0;
+		p2.misses = 0;
 	}
-	if (ball.x <= 16 && ball.y >= p1.y && ball.y <= p1.y + 40) {
+	if (ball.x <= 16 && ball.y >= p1.y && ball.y <= p1.y + p1.height) {
 		ball.dx *= -1;
-		ball_miss = 0;
+		p1.misses = 0;
 	}
 	//if ball goes out of bounds, gives pts and respawns
 	if(ball.x > 490) {
@@ -144,7 +144,7 @@ function gameTick() {
  		ball.x = ball.spawn_x-offset;
  		ball.y = ball.spawn_y;
 		document.getElementById("player1-score").innerHTML = p1.score;
-		ball_miss +=1;
+		p2.misses +=1;
 	}
  	if(ball.x < 10) {
     	p2.score += 1;
@@ -152,7 +152,7 @@ function gameTick() {
 		ball.x = ball.spawn_x+offset;
 		ball.y = ball.spawn_y;
 		document.getElementById("player2-score").innerHTML = p2.score;
-		ball_miss += 1;
+		p1.misses += 1;
 	}
 	(ball.dx<0)? ball.dx-=ball.inc:ball.dx+=ball.inc;
 	ball.style.top=ball.y;
