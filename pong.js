9d1bc68e9abbea159d57ca88a5a2cf050895e941
niharmitra <Nihar@niharmitrasimac.hsd1.ca.comcast.net>
2014-02-16 12:47:15 -0800

    Changing from CSS to HTML 5 Canvas

--- pong.js
+++ pong.js
@@ -1,16 +1,29 @@
+/*
+TABLE OF CONTENTS:
+1. Constructors for game objects
+2. Global variables
+3. Functions and event handlers to be called at the start
+4. New Game setup
+5. Settings and associated functions
+6. Game running functions
+7. Input receivers
+*/
+
 //Constructor for paddles
-function paddle(element_id, y_df, dy_df, speed_df, height_df, score_df, scorebox_id, namebox_id) {
-	this.style = document.getElementById(element_id).style;
+function paddle(y, speed, width, height, score_df, scorebox_id, namebox_id) {
+	//default values (by me)
+	this.x_df = 0;
+	this.y_df = 0;
+	this.speed_df = 2.0;
+	this.height_df = 40;
+	this.width_df = 7;
 
-	this.y_df = y_df;
-	this.dy_df = dy_df;
-	this.speed_df = speed_df;
-	this.height_df = height_df;
-
-	this.y = y_df;
-	this.dy = dy_df;
-	this.speed = speed_df;
-	this.height = height_df;
+	//values set by user if changed
+	this.y = y;
+	this.dy = 0;
+	this.speed = speed;
+	this.height = height;
+	this.width = width
 
 	this.set = paddleSet;
 	this.score = score_df;
@@ -38,13 +51,11 @@ function paddleSet(paddle_settings) {
 }
 
 //Creates paddles
-var p1 = new paddle("paddle1", 230, 0, 2.0, 40, 0, "player1-score", "player1-name");
-var p2 = new paddle("paddle2", 230, 0, 2.0, 40, 0, "player2-score", "player2-name");
+var p1 = new paddle(230, 2.0, 7, 40, 0, "player1-score", "player1-name");
+var p2 = new paddle(230, 2.0, 7, 40, 0, "player2-score", "player2-name");
 
 //Constructor for Ball Object
 function ballConstructor(spawn_x_df, spawn_y_df, dx_df, dy_df, inc_df) {
-	this.style = document.getElementById("ball").style;
-
 	this.spawn_x_df = parseFloat(spawn_x_df);
 	this.spawn_y_df = parseFloat(spawn_y_df);
 	this.spawn_x = parseFloat(spawn_x_df);
@@ -66,13 +77,19 @@ function ballConstructor(spawn_x_df, spawn_y_df, dx_df, dy_df, inc_df) {
 var ball = new ballConstructor(247, 247, 0.8, 1.1, 0.0000000000001);
 var ball_miss; //counts how many times the ball has been missed by a particular player
 
-//Settings Declared
+//GLOBAL VARIABLES
 var basic_settings;
 var ball_settings;
 var paddle1_settings;
 var paddle2_settings;
 var new_name;
 var advanced = false; //advanced settings or not
+
+
+//CANVAS ELEMENTS
+var cvs = document.getElementById("game_area");
+var ctx = cvs.getContext("2d");
+ctx.fillStyle = "white";
 
 /*
 p1 = Paddle 1, p2 = paddle 2
@@ -98,63 +115,13 @@ function playerName() {
 var text_boxes = document.querySelectorAll("input[type=text]");
 for(var i=0; i<text_boxes.length; i++) {
 	text_boxes[i].onclick = function() {
-		if(typeof text_boxes[i] !="undefined") {
+		if(typeof text_boxes[i]!="undefined") {
 			text_boxes[i].value="";
 		}
 	}
 }
 
-//shows advanced settings
-function showAdvanced() {
-	advanced = !advanced;
-	if(advanced) {
-		document.getElementById("paddle_length").innerHTML = "Paddle 1 Length:";
-		document.createElement("input");
-	}
-	else {
-		document.getElementById("paddle_length").innerHTML = "Paddle Length:";
-	}
-}
-
-function updateScore() {
-	if(p1.score==p2.score) {
-		p1.scorebox.innerHTML = p1.score;
-		p2.scorebox.innerHTML = p2.score;
-		return;
-	}
-	var winning = p1.score>p2.score?true:false;
-	if(winning) {
-		p1.scorebox.innerHTML = "<highlight>"+p1.score+"</highlight>";
-		p2.scorebox.innerHTML = p2.score;
-		return;
-	}
-	else {
-		p1.scorebox.innerHTML = p1.score;
-		p2.scorebox.innerHTML = "<highlight>"+p2.score+"</highlight>";
-		return;
-	}
-}
-
-function convertInput(input, default_val) {
-	input = parseFloat(input);
-	if(input == NaN) {
-		return default_val;
-	}
-	else {
-		return input;
-	}
-}
-
-function resetSettings() {
-	ball_settings = document.getElementsByName("ball_settings");
-	paddle1_settings = document.getElementsByName("paddle1_settings");
-	basic_settings = document.getElementsByName("basic_settings");
-
-	basic_settings[0].checked = false;
-	ball_settings[0].value = 0.8;
-	paddle1_settings[0].value = 40;
-}
-
+//SETTING UP A NEW GAME:
 function newGame() {
 	//settings from form
 	ball_settings = document.getElementsByName("ball_settings");
@@ -186,7 +153,58 @@ function newGame() {
 
 	return false;
 }
+//SETTINGS:
+//HIDES/REVEALS ADVANCED SETTINGS
+function showAdvanced() {
+	advanced = !advanced;
+	if(advanced) {
+		document.getElementById("paddle_length").innerHTML = "Paddle 1 Length:";
+		document.createElement("input");
+	}
+	else {
+		document.getElementById("paddle_length").innerHTML = "Paddle Length:";
+	}
+}
 
+function convertInput(input, default_val) {
+	input = parseFloat(input);
+	if(input == NaN) {
+		return default_val;
+	}
+	else {
+		return input;
+	}
+}
+
+function resetSettings() {
+	ball_settings = document.getElementsByName("ball_settings");
+	paddle1_settings = document.getElementsByName("paddle1_settings");
+	basic_settings = document.getElementsByName("basic_settings");
+
+	basic_settings[0].checked = false;
+	ball_settings[0].value = 0.8;
+	paddle1_settings[0].value = 40;
+}
+function updateScore() {
+	if(p1.score==p2.score) {
+		p1.scorebox.innerHTML = p1.score;
+		p2.scorebox.innerHTML = p2.score;
+		return;
+	}
+	var winning = p1.score>p2.score?true:false;
+	if(winning) {
+		p1.scorebox.innerHTML = "<highlight>"+p1.score+"</highlight>";
+		p2.scorebox.innerHTML = p2.score;
+		return;
+	}
+	else {
+		p1.scorebox.innerHTML = p1.score;
+		p2.scorebox.innerHTML = "<highlight>"+p2.score+"</highlight>";
+		return;
+	}
+}
+
+//RUNNING GAME
 function gameTick() {
 	ball.x += parseFloat(ball.dx);
 	//Why is ball.dx initially null? Why does ball.x go 247 to 2471e-13.
@@ -194,9 +212,6 @@ function gameTick() {
 	//makes sure paddle doesn't go off screen
 	p1.y = Math.min(Math.max(p1.y + p1.dy, 0), 460);
 	p2.y = Math.min(Math.max(p2.y + p2.dy, 0), 460);
-	
-	p1.style.top=p1.y;
-	p2.style.top=p2.y;
 	
 	var offset = Math.floor(Math.random()*50);
 
@@ -237,15 +252,29 @@ function gameTick() {
 		ball.dx+=ball.inc;
 	}
 
-	ball.style.top = ball.y;
-	ball.style.left = ball.x;
-	
+	paint();
 	setTimeout('gameTick()', 1);
+}
+
+//paints screen accordingly
+function paint() {
+	var width = cvs.width;
+	var height = cvs.height;
+
+	//draws paddles
+	//subtracts .y from height b/c origin is upper left corner
+	ctx.fillRect(10, height-p1.y, p1.width, 40);
+	ctx.fillRect(width-10, height-p2.y, p2.width, 40);
+	
+	//draws ball
+	//compensates for ball.x and ball.y being from the center of the ball
+	ctx.fillRect( (ball.width)/2-ball.x, height-ball.y+(ball.height/2), ball.width, ball.height);
 }
 
 playerName();
 newGame();
 
+//GAME INPUT RESPONSE
 //changes paddle speeds according to key presses
 document.onkeydown = function(e) {
 	if(e.keyCode == 83) {
